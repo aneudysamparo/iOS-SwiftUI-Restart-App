855ec6cd0f50d90327bbe035624e5fd18fb7cdcd
Aneudys Amparo <aamparo@kambda.com>
2023-04-25 20:41:44 -0400

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Aneudys Amparo on 25/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
